046000eb45e0694868ad07f2e3b446c14a923c7c
ChrisGuenther333 <CHRIS.GUENTHER4328@STU.MTEC.EDU>
2024-03-29 11:00:56 -0600

    Added basic page layout reference stylings

--- react-concert-app/app/Components/Page Layout/ConcertList.jsx
+++ react-concert-app/app/Components/Page Layout/ConcertList.jsx
@@ -1,12 +1,22 @@
 export default function ConcertList() {
   return (
-    <div className="w-1/3 bg-zinc-500">
+    <div className="text-center w-1/3 h-svh bg-zinc-500">
       <h1>Concert List</h1>
-      <div id="buttons" className="flex">
-        <button id="add-concert">+ Add Concert</button>
-        <button id="search-concert">Search</button>
+      <div id="buttons" className="flex justify-around mb-5">
+        <button id="add-concert" className="p-2 bg-white">+ Add Concert</button>
+        <button id="search-concert" className="p-2 bg-white">Search</button>
       </div>
-      <div className="flex">
+      <div className="flex justify-around border py-1">
+        <h4>Date</h4>
+        <h4>Artist/Band</h4>
+        <h4>Venue</h4>
+      </div>
+      <div className="flex justify-around border py-1">
+        <h4>Date</h4>
+        <h4>Artist/Band</h4>
+        <h4>Venue</h4>
+      </div>
+      <div className="flex justify-around border py-1">
         <h4>Date</h4>
         <h4>Artist/Band</h4>
         <h4>Venue</h4>
--- react-concert-app/app/Components/Page Layout/DisplayedConcert.jsx
+++ react-concert-app/app/Components/Page Layout/DisplayedConcert.jsx
@@ -1,16 +1,12 @@
 export default function DisplayedConcert() {
     return (
-      <div className="w-2/3 bg-zinc-500">
+      <div className="w-2/3 h-svh text-center bg-stone-200">
         <h1>Displayed Concert</h1>
-        <div id="buttons" className="flex">
-          <button id="add-concert">+ Add Concert</button>
-          <button id="search-concert">Search</button>
-        </div>
-        <div className="flex">
-          <h4>Date</h4>
+        <div className="flex justify-around">
           <h4>Artist/Band</h4>
           <h4>Venue</h4>
         </div>
+        <h5>Date</h5>
       </div>
     );
   }
