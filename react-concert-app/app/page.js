cef3fa2c768d596b4ae45612476883f6cb490470
ChrisGuenther333 <CHRIS.GUENTHER4328@STU.MTEC.EDU>
2024-03-29 08:37:02 -0600

    Git test

--- react-concert-app/app/page.js
+++ react-concert-app/app/page.js
@@ -4,8 +4,8 @@ import TableLeft from "./components/TableLeft.jsx";
 export default function Home() {
   return (
     <div>
-      <h1>Chris Display Test</h1>
-      <p>Very cool</p>
+      <h1>Chris Git Test 2</h1>
+      <p>Very nice</p>
       <div className="flex justify-evenly">
       <div>**table here**</div>
       <div>**table here**</div>
