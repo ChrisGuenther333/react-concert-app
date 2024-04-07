28cf3ed3b7e73d7b0880d5e30a5b9a525cdc09b6
Nicole Linnarz <mikaelafs17@gmail.com>
2024-04-07 11:21:30 -0600

    changed "searc" to input and different component

--- react-concert-app/app/Components/Elements/Inputs.jsx
+++ react-concert-app/app/Components/Elements/Inputs.jsx
@@ -1,16 +1,9 @@
-// import { useState } from "react";
-
-export default function AddConcertBtn() {
-  // const [input, showInput] = useState(0);
-
-  // const active = () => {};
-
+export default function SearchConcertInput() {
   return (
-    <button
-      id="add-concert"
+    <input
+      id="search-concert"
       className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-    >
-      + Add Concert
-    </button>
+      placeholder="Search..."
+    />
   );
 }
